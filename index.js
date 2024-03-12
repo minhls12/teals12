1db92767c10a1c588766e7fd7d197d5385134f03
Lg Minh <141997860+minhls12@users.noreply.github.com>
2024-03-13 02:11:25 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Main function to perform the operation
+function calculateProduct(x, y) {
+  const product = x * y;
+  return product;
+}
+
+// Example of usage:
+const number1 = 3;
+const number2 = 5;
+const product = calculateProduct(number1, number2);
+
+console.log(`${number1} * ${number2} = ${product}`);
